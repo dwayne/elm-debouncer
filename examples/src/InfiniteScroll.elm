9763a1abbcf6c56d6c49052db15eb5fb7ce0395a
Dwayne Crooks <me@dwaynecrooks.com>
2023-04-04 20:47:34 -0400

    Implement the infinite scrolling example from the CSS-Tricks article

--- examples/src/InfiniteScroll.elm
+++ examples/src/InfiniteScroll.elm
@@ -1,6 +1,10 @@
 module InfiniteScroll exposing (main)
 
+-- This example is based on
+-- https://css-tricks.com/debouncing-throttling-explained-examples/#aa-infinite-scrolling.
+
 import Browser as B
+import Debouncer exposing (Debouncer)
 import Html as H
 import Html.Attributes as HA
 import Html.Events as HE
@@ -23,6 +27,7 @@ main =
 
 type alias Model =
     { blocks : List Int
+    , debouncer : Debouncer ScrollEvent
     }
 
 
@@ -34,6 +39,7 @@ blocksPerPage =
 init : () -> ( Model, Cmd msg )
 init _ =
     ( { blocks = blocksPerPage
+      , debouncer = Debouncer.throttle 300
       }
     , Cmd.none
     )
@@ -45,16 +51,53 @@ init _ =
 
 type Msg
     = Scrolled ScrollEvent
+    | ReadyToCheck ScrollEvent
+    | ChangedDebouncer (Debouncer.Msg Msg ScrollEvent)
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Scrolled event ->
-            Debug.log (Debug.toString event) <|
+            let
+                ( debouncer, cmd ) =
+                    Debouncer.call
+                        { onReady = ReadyToCheck
+                        , onChange = ChangedDebouncer
+                        }
+                        event
+                        model.debouncer
+            in
+            ( { model | debouncer = debouncer }
+            , cmd
+            )
+
+        ReadyToCheck { sceneHeight, viewportY, viewportHeight } ->
+            let
+                distance =
+                    sceneHeight - viewportY - viewportHeight
+            in
+            if distance < 200 then
+                ( { model | blocks = List.append blocksPerPage model.blocks }
+                , Cmd.none
+                )
+
+            else
                 ( model
                 , Cmd.none
                 )
+
+        ChangedDebouncer debouncerMsg ->
+            let
+                ( debouncer, cmd ) =
+                    Debouncer.update
+                        ChangedDebouncer
+                        debouncerMsg
+                        model.debouncer
+            in
+            ( { model | debouncer = debouncer }
+            , cmd
+            )
 
 
 
